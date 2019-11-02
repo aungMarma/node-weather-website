1f097a325a9707ff3d86a90e8e7fa2735bc0201c
AUNGKYAWCHA MARMA <aungkyawcha@AUNGKYAWCHAsMBP.home>
2019-11-01 23:05:21 -0400

    Set up app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener('submit', (e) => {
 	messageFirst.textContent = 'Loading message!';
 	messagSecond.textContent = '';
 	const location = search.value;
-	fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+	fetch(`/weather?address=${location}`).then((response) => {
 		response.json().then((data) => {
 			if (data.error) {
 				messageFirst.textContent = data.error;
--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const path = require('path');
 const hbs = require('hbs');
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000; // Heroku or local port
 
 // Define paths for Express config
 const staticsPath = path.join(__dirname, '../public');
@@ -44,7 +45,6 @@ app.get('/about', (req, res) => {
 
 app.get('/weather', (req, res) => {
 	const address = req.query.address;
-	console.log(req.query);
 	if (!address) {
 		return res.send({
 			error: 'You must provide an address.'
@@ -66,18 +66,6 @@ app.get('/weather', (req, res) => {
 		});
 	});
 });
-
-// app.get('/products', (req, res) => {
-// 	if (!req.query.search) {
-// 		return res.send({
-// 			// withou return the execution of get function doesn't stop here
-// 			message: 'You must provide a search term.'
-// 		});
-// 	}
-// 	res.send({
-// 		products: []
-// 	});
-// });
 
 app.get('/help', (req, res) => {
 	res.render('help', {
@@ -104,6 +92,6 @@ app.get('*', (req, res) => {
 	});
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
 	console.log('Server is up on port 3000');
 });
